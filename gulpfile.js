50c586a2c59aa7f8716d69e5241c03a7ec1d5171
= <=>
2017-06-06 16:00:04 -0300

    Adição do Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,59 @@
 var gulp = require('gulp'),
-	imagemin = require('image-min');
+	imagemin = require('gulp-imagemin'),
+	clean= require('gulp-clean'),
+	concat = require('gulp-concat'),
+	htmlReplace = require('gulp-html-replace'),
+	uglify = require('gulp-uglify'),
+	usemin = require('gulp-usemin'),
+	cssmin = require('gulp-cssmin'),
+	cssStripComment = require('gulp-strip-css-comments');
 
+
+
+gulp.task('copy', ['clean'], function(){
+	return gulp.src('src/**/*')
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean',function(){
+	return gulp.src('dist')
+		.pipe(clean())
+});
 gulp.task('build-img', function(){
 
-gulp.src('src/images/**/*')
+gulp.src('dist/images/**/*')
 	.pipe(imagemin())
-	.pipe(gulp.dest('src/images'))
+	.pipe(gulp.dest('dist/images'))
 	
+});
+
+gulp.task('build-js',function(){
+	gulp.src(['dist/js/jquery.js','dist/js/bootstrap.min.js','dist/js/jquery.isotope.min.js',
+		'dist/js/lightbox.min.js','dist/js/wow.min.js','dist/js/jquery.countTo.js','dist/js/main.js'])
+	.pipe(concat('all.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('build-html',function(){
+	gulp.src('dist/**/*.html')
+	.pipe(htmlReplace({
+		js: 'js/all.js'
+	}))
+	.pipe(gulp.dest('dist'))
+});
+gulp.task('usemin',function(){
+	
+	gulp.src('dist/**/*.html')
+		.pipe(usemin({
+			'js' : [uglify],
+			'css' :[cssmin]
+		}))
+		.pipe(gulp.dest('dist'))
+		gulp.src('dist/css/*.css').pipe(cssStripComment()).pipe(gulp.dest('dist'))
 })
 
+
+gulp.task('default',['copy'],function(){
+	gulp.start('build-img','usemin')
+});
