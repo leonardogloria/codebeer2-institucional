1b5d9d9582919ea7a89deb9e1b9a19401c2b5e3b
= <=>
2017-06-08 15:22:34 -0300

    Versão 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var gulp = require('gulp'),
-	imagemin = require('gulp-imagemin'),
-	clean= require('gulp-clean'),
-	concat = require('gulp-concat'),
-	htmlReplace = require('gulp-html-replace'),
-	uglify = require('gulp-uglify'),
-	usemin = require('gulp-usemin'),
-	cssmin = require('gulp-cssmin'),
-	cssStripComment = require('gulp-strip-css-comments'),
-	revAll = require('gulp-rev-all'),
-	awspublish = require('gulp-awspublish'),
-	cloudfront = require('gulp-cloudfront'),
-	browserSync = require('browser-sync');
+var gulp = require('gulp')
+	,imagemin = require('gulp-imagemin')
+	,clean= require('gulp-clean')
+	,concat = require('gulp-concat')
+	,htmlReplace = require('gulp-html-replace')
+	,uglify = require('gulp-uglify')
+	,usemin = require('gulp-usemin')
+	,cssmin = require('gulp-cssmin')
+	,cssStripComment = require('gulp-strip-css-comments')
+	,revAll = require('gulp-rev-all')
+	,awspublish = require('gulp-awspublish')
+	,cloudfront = require('gulp-cloudfront')
+	,browserSync = require('browser-sync')
+	,fs = require("fs")
+	,stripComment = require('gulp-strip-comments');
 
 
 
-
-var headers = {'Cache-Control': 'max-age=315360000, no-transform, public'};
 gulp.task('serve',function(){
 	browserSync.init({
 		server: {
@@ -41,70 +41,69 @@ return gulp.src('dist/images/**/*')
 	.pipe(gulp.dest('dist/images'))
 	
 });
-
+gulp.task('strip-css',['usemin'],function(){
+gulp.src('dist/**/*.css').pipe(cssStripComment())
+		.pipe(gulp.dest('dist'))
+});
+gulp.task('strip-js',['usemin'],function(){
+gulp.src('dist/**/*.js')
+		.pipe(stripComment())
+		.pipe(gulp.dest('dist'))
+});
+gulp.task('strip-html',['usemin'],function(){
+gulp.src('dist/**/*.html')
+		.pipe(stripComment())
+		.pipe(gulp.dest('dist'))
+});
 
 gulp.task('usemin',function(){
 	
-	gulp.src('dist/**/*.html')
+	return gulp.src('dist/**/*.html')
 		.pipe(usemin({
 			'js' : [uglify],
 			'css' :[cssmin]
 		}))
 		.pipe(gulp.dest('dist'))
-		gulp.src('dist/css/*.css').pipe(cssStripComment()).pipe(gulp.dest('dist'))
+		
 })
 
-gulp.task('publish',function(){
+gulp.task('deploy',['build-img'],function(){
+	var headers = {'Cache-Control': 'max-age=315360000, no-transform, public'};
+	var contents = fs.readFileSync("aws-credentials.json");
+	var credentials = JSON.parse(contents);
 
-	 var publisher = awspublish.create({
+	var aws = {
 	    params: {
-	      Bucket: 'codebeer2.com.br'
+	      Bucket: credentials.bucket
 	    },
-	    region: 'sa-east-1',
-	    accessKeyId: "xxx",
-  		secretAccessKey: "xxx",
-		signatureVersion: 'v3',
-		 distributionId: "xxx"
-
-
-	  });
-	 var aws = {
-	    params: {
-	      Bucket: 'codebeer2.com.br'
-	    },
-	    region: 'sa-east-1',
-	    accessKeyId: "xxx",
-  		secretAccessKey: "xxx",
-		signatureVersion: 'v3',
-		 distributionId: "xxx"
+	    region: credentials.region,
+	    accessKeyId: credentials.accessKeyId,
+  		secretAccessKey: credentials.secretAccessKey,
+		signatureVersion: credentials.signatureVersion,
+		distributionId: credentials.distributionId
 
 
 	  };
 
-
-	   var headers = {
+	var publisher = awspublish.create(aws);
+	var headers = {
     		'Cache-Control': 'max-age=315360000, no-transform, public'
   
   		};
   		gulp.src(['dist/**/*'])
   		.pipe(awspublish.gzip())
   		.pipe(publisher.publish(headers))
-  		//.pipe(publisher.cache())
+  		.pipe(publisher.cache())
   		.pipe(awspublish.reporter())
         .pipe(cloudfront(aws));
 
-
-	 /*gulp.src('dist/**')
-        .pipe(revAll.revision())
-        .pipe(awspublish.gzip())
-        .pipe(publisher.publish(headers))
-        .pipe(publisher.cache())
-        .pipe(awspublish.reporter())
-        .pipe(cloudfront(aws));
-
-        */
-
 });
 gulp.task('default',['copy'],function(){
-	gulp.start('build-img','usemin','publish')
+	gulp.start('build-img','usemin','strip-html','strip-css','strip-js')
 });
+
+gulp.task('default-prod',['copy'],function(){
+	gulp.start('build-img','usemin','strip-html','strip-css','strip-js','deploy')
+});
+
+
